d19d6540f54ad8922665e270e3296aac23e5dd60
vyu <vyuvyuvyu@naver.com>
2024-06-10 01:09:21 +0900

    feat: fetch data with react method

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,29 @@
-export const metadata = {
-  title: 'Home',
-};
+'use client';
+import { useEffect, useState } from 'react';
 
-export default function Tomato() {
+// export const metadata = {
+//   title: 'Home',
+// };
+
+export default function Page() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMoives] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      'https://nomad-movies.nomadcoders.workers.dev/movies'
+    );
+    const json = await response.json();
+    setMoives(json);
+  };
+  useEffect(() => {
+    getMovies();
+    setIsLoading(false);
+  }, []);
+
   return (
     <div>
       <h1>Hello NextJS!</h1>
+      <h1>{isLoading ? 'Loading...' : JSON.stringify(movies)}</h1>
     </div>
   );
 }
