c432de00fe12c4a61ef5f5aead1c44c84ec36e41
vyu <vyuvyuvyu@naver.com>
2024-06-10 02:28:20 +0900

    feat: loading pages and promise all fetch movie and videos

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,22 @@
+import { API_URL } from '../../../(home)/page';
+
 type props = {
   params: { id: string };
 };
 
-export default function MovieDetail({ params: { id } }: props) {
-  return <h1>Movie {id}</h1>;
+async function getMovie(id: string) {
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+async function getVideos(id: string) {
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
+export default async function MovieDetail({ params: { id } }: props) {
+  // const movie = await getMovie(id);
+  // const videos = await getVideos(id);
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  return <h1>{movie.title}</h1>;
 }
