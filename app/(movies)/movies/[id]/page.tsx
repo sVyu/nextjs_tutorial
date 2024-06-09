33005cdaf3ffeef540113f008308dd1e89d4fae2
vyu <vyuvyuvyu@naver.com>
2024-06-10 02:52:49 +0900

    feat: make use of React Suspense for loading and streaming component

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,22 +1,23 @@
+import { Suspense } from 'react';
 import { API_URL } from '../../../(home)/page';
+import MovieInfo from '../../../../components/movie-info';
+import MovieVideos from '../../../../components/movie-videos';
 
 type props = {
   params: { id: string };
 };
 
-async function getMovie(id: string) {
-  const response = await fetch(`${API_URL}/${id}`);
-  return response.json();
-}
-
-async function getVideos(id: string) {
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
-}
-
 export default async function MovieDetail({ params: { id } }: props) {
-  // const movie = await getMovie(id);
-  // const videos = await getVideos(id);
-  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
-  return <h1>{movie.title}</h1>;
+  // const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  return (
+    <div>
+      <h3>Movie detail page</h3>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
