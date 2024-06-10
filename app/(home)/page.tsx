812ce47a4c88124638e1b4bc7d17f26d28ed057c
vyu <vyuvyuvyu@naver.com>
2024-06-10 18:10:51 +0900

    refactor: delete unnecessary comments

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -8,8 +8,6 @@ export const metadata = {
 export const API_URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
 async function getMovies() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
-
-  // return fetch(API_URL).then((response) => response.json());
   const response = await fetch(API_URL);
   const json = await response.json();
   return json;
@@ -17,7 +15,6 @@ async function getMovies() {
 
 export default async function HomePage() {
   const movies = await getMovies();
-  // return <div>{JSON.stringify(movies)}</div>;
   return (
     <div className={styles.container}>
       {movies.map((movie) => (
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -5,12 +5,10 @@ async function getVideos(id: string) {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(`${API_URL}/${id}/videos`);
   return response.json();
-  // throw new Error('something broke...');
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
-  // return <h6>{JSON.stringify(videos)}</h6>;
   return (
     <div className={styles.container}>
       {videos.map((video) => (
