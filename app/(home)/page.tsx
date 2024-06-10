8c72036519905d7a7ce372c189ab72fb2e1c86ff
vyu <vyuvyuvyu@naver.com>
2024-06-10 17:29:41 +0900

    reafctor: home(movies) css and movie component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,5 @@
-import Link from 'next/link';
+import Movie from '../../components/movie';
+import styles from '../../styles/home.module.css';
 
 export const metadata = {
   title: 'Home',
@@ -6,7 +7,6 @@ export const metadata = {
 
 export const API_URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
 async function getMovies() {
-  // console.log('im fetching');
   await new Promise((resolve) => setTimeout(resolve, 2000));
 
   // return fetch(API_URL).then((response) => response.json());
@@ -19,11 +19,14 @@ export default async function HomePage() {
   const movies = await getMovies();
   // return <div>{JSON.stringify(movies)}</div>;
   return (
-    <div>
+    <div className={styles.container}>
       {movies.map((movie) => (
-        <li key={movie.id}>
-          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          poster_path={movie.poster_path}
+          title={movie.title}
+        />
       ))}
     </div>
   );
